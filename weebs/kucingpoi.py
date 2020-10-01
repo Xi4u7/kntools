c8204e36d2e6e29f5161a0358d1fc2926ca4b5ea
Diky AR <androxghost1337@hotmail.com>
2020-10-02 05:57:55 +0700

    Update kucingpoi.py

--- weebs/kucingpoi.py
+++ weebs/kucingpoi.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import requests, re
+linkpoi = raw_input('linkpoi.cc: ')
 s = requests.session()
-req1 = s.get("https://linkpoi.cc/it8zl").text
+req1 = s.get(linkpoi).text
 get1 = re.findall('<a href="(.*?)" class="btn btn-primary btn-block redirect" rel="nofollow">Menuju Link</a></center>', req1)[0]
 req2 = s.get(get1).text
 formpost = re.findall('<form method="POST" action="(.*?)"', req2)[0]
@@ -12,5 +13,16 @@ formpost = re.findall('<form method="POST" action="(.*?)"', req3)[0]
 _token = re.findall('<input name="_token" type="hidden" value="(.*?)">', req2)[0]
 data2 = {'_token': _token, 'x-token': ''}
 req4 = s.post(formpost, data=data2, allow_redirects=False)
-print req4.headers["Location"]
-print data2
+url = req4.headers["Location"]
+
+text = requests.get(url)
+get_zippy_url = re.findall('"text": "(.*?)/file.html"', text.text)[0]
+get_unicode = re.findall("\"\+\((.*?)%1000", text.text)[0]
+omg_class = 2
+a = 1
+b = a + 1
+c = b + 1
+d = omg_class * 2
+title = re.findall('5\/5\)\+"\/(.*?)";', text.text)[0]
+asu = get_zippy_url.replace("/v/","/d/")+"/"+str(int(get_unicode)%1000 + a + b + c + d + 5/5)+"/"+title
+print asu
